2aba6c07d1156665ef237014f8475387eb8bb34e
Lenovo <3153566913@qq.com>
2023-08-03 21:06:01 +0800

    8.3

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -152,7 +152,7 @@ export const zhSidebar = sidebar({
             collapsible: true,
             prefix:"/projects/zatan/8",
             children:[
-                "1.md","2.md","3.md"
+                "1.md","2.md","3.md","4.md"
             ]
         }
     ],

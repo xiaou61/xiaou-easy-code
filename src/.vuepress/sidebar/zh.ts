39958b284de04dfc29a69d709ab52278dc61bd4b
Lenovo <3153566913@qq.com>
2023-08-10 09:56:12 +0800

    8.10

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -21,7 +21,7 @@ export const zhSidebar = sidebar({
                     collapsible: true,
                     icon: "write",
                     prefix: "qf/",
-                    children: ["1.md","2.md","3.md"]
+                    children: ["1.md","2.md","3.md","4.md"]
                 },
                 {
                     text: "JVM(尚硅谷)",
@@ -145,7 +145,7 @@ export const zhSidebar = sidebar({
                     collapsible: true,
                     prefix: "8/",
                     children: [
-                        "1.md","2.md","3.md"
+                        "1.md","2.md","3.md","4.md"
                     ]
                 }
             ],
@@ -187,7 +187,7 @@ export const zhSidebar = sidebar({
             collapsible: true,
             prefix: "/projects/zatan/8",
             children: [
-                "1.md", "2.md", "3.md", "4.md", "5.md", "6.md","7.md","8.md","9.md"
+                "1.md", "2.md", "3.md", "4.md", "5.md", "6.md","7.md","8.md","9.md","10.md"
             ]
         }
     ],

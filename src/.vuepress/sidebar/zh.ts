3f8079072eb59abcd54f495a9d5de12c0514b5be
李子凡 <3153566913@qq.com>
2023-09-02 15:39:46 +0800

    9.1

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -168,7 +168,7 @@ export const zhSidebar = sidebar({
                     collapsible: true,
                     prefix: "9/",
                     children: [
-                        "1.md"
+                        "1.md","2.md"
                     ]
                 }
             ],

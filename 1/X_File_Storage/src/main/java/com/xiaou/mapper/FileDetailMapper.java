0f7666be00441cf2c1d01479dcf8d5d6e2004ae5
aliyun1153623907 <3153566913@qq.com>
2024-05-03 21:37:38 +0800

    1.优化了文件上传

---
+++ 1/X_File_Storage/src/main/java/com/xiaou/mapper/FileDetailMapper.java
@@ -0,0 +1,7 @@
+package com.xiaou.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaou.domain.FileDetail;
+
+public interface FileDetailMapper extends BaseMapper<FileDetail> {}
